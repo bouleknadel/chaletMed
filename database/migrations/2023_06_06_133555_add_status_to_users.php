2c058f5321606294ff3981a8eb6ed09c3bac97aa
FHK <fhassoumikadri@gmail.com>
2023-06-07 15:14:08 +0100

    add_status_to_users

---
+++ database/migrations/2023_06_06_133555_add_status_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('status')->default('ACTIVE');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('status', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
